baac09290113365688bec1105a939be40bae4dd8
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-01 15:14:36 +0530

    Q4

---
+++ day 01/Q4/Q4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,fn1=0,fn2=1,output;
+            Console.Write("Enter Number -: ");
+            n = int.Parse(Console.ReadLine());  
+
+              if(n>=1)
+              {
+                   Console.Write(fn1 + " ");
+              }
+
+                 if(n>=2)
+                 {
+                      Console.Write(fn2 + " ");
+                 }
+                   do
+                   {
+                      output = fn1 + fn2;
+                      fn1 = fn2;
+                      fn2 = output;
+                      n--;
+                      Console.Write(output + " ");
+                   } while (n > 1);
+            Console.ReadLine();   
+        }
+    }
+}
