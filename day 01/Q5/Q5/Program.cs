603c944216110f0661005951723e6a78e42628ff
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-02 21:54:52 +0530

    Q6

---
+++ day 01/Q5/Q5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mult;
+            Console.Write("Enter A number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= 12; i++)
+            {
+              mult = num * i;
+              Console.WriteLine( num + "*" + i + "=" + mult );
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ day 01/Q6/Q6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Name -: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter The Marks -: ");
+            int marks = int.Parse(Console.ReadLine());
+
+           if (marks >100||marks<0)
+            {
+                Console.WriteLine("Invalid Marks");
+            }
+           else if (marks >= 75)
+            {
+                Console.WriteLine(name + " Grade A");
+            }
+           else if (marks >= 60)
+            {
+                Console.WriteLine(name + " Grade B");
+            }
+           else if (marks >= 50)
+            {
+                Console.WriteLine(name + " Grade C");
+            }
+           else if (marks >= 40)
+            {
+                Console.WriteLine(name + " Grade D");
+            }
+           else
+            {
+                Console.WriteLine(name + " Grade F");
+            } 
+           Console.ReadLine();
+        }
+    }
+}
