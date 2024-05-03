371c92f164c7701e8aaadc966469d8cc6f9ceac2
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-04 00:00:26 +0530

    Q4 half changes

---
+++ day 02/Q4/Q4/product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class product
+    {
+        public string ProductName;
+        public string Price;
+    }
+}
