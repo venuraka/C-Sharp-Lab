129dd0f578d6b13e63fa0bf407bbdedf7304436e
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-03 22:36:34 +0530

    Q2

---
+++ day 02/Q2/Q2/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class BankAccount
+    {
+        public int AccountNumber;
+        public int Balance=1000;
+
+        public void Deposit()
+        {
+            Console.Write("Enter the amount to deposit: ");
+            int amount = int.Parse(Console.ReadLine());
+            Balance=Balance+amount;
+            Console.WriteLine("Amount deposited successfully,your balance is "+Balance);
+
+        }
+    }
+}
---
+++ day 02/Q2/Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the account number -: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            BankAccount account1 = new BankAccount();
+            account1.AccountNumber = accountNumber;
+
+            account1.Deposit();
+
+            Console.ReadLine();
+        }
+    }
+}
