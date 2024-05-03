ca33f8ee475845d22880e2a78ac4114a81dffe19
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-03 23:53:17 +0530

    Q3

---
+++ day 02/Q3/Q3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker track = new TemperatureTracker(); // Instantiate TemperatureTracker
+
+            string[] daysOfTheWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            for (int i = 0; i < daysOfTheWeek.Length; i++)
+            {
+                Console.Write("Enter temperature for " + daysOfTheWeek[i] + ":- ");
+                double temp = double.Parse(Console.ReadLine()); // Read temperature as double
+                track.SetTemperature(i, temp); // Store temperature for the current day
+            }
+
+            // Display the weekly report
+            track.DisplayWeeklyReport();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ day 02/Q3/Q3/TemperatureTracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class TemperatureTracker
+    {
+        private double[] temperatures = new double[7]; // Array to store temperatures for each day of the week
+
+        public void SetTemperature(int dayIndex, double temperature)
+        {
+            if (dayIndex >= 0 && dayIndex < temperatures.Length)
+            {
+                temperatures[dayIndex] = temperature;
+            }
+            else
+            {
+                Console.WriteLine("Invalid day index!");
+            }
+        }
+
+        public void DisplayWeeklyReport()
+        {
+            Console.WriteLine("Weekly Temperature Report:");
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {temperatures[i]}°C");
+            }
+        }
+    }
+}
+  
