b666363de4ecb4ec186ad361760bd8962c7e13bf
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-04 16:14:01 +0530

    Q5

---
+++ day 02/Q5/Q5/LibraryBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class LibraryBook
+    {
+        public string Title;
+        public string Author;
+        public bool Available;
+
+
+        public void BorrowBook(bool Available)
+        {
+            if (Available)
+            {
+                Available = false;
+                Console.WriteLine("Book borrowed.");
+            }
+            else
+            {
+                Console.WriteLine("Book not available.");
+            }
+        }
+    }
+    }
---
+++ day 02/Q5/Q5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryBook book1 = new LibraryBook();
+          
+
+            Console.Write("Enter the tiltle of the book-: ");
+            string title = Console.ReadLine();
+            title = book1.Title;
+            
+            Console.Write("Is this book available (true/false): ");
+            bool available = bool.Parse(Console.ReadLine());
+            book1.BorrowBook(available);
+            Console.ReadLine();
+        }
+    }
+}
