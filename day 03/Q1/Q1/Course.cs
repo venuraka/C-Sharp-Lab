fc6410d36520065f00cbaeb2a8262795e1244d97
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2024-05-05 19:15:59 +0530

    Q1

---
+++ day 03/Q1/Q1/Course.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Q1
+{
+    internal class Course
+    {
+        private string courseName;
+        private string instructorName;
+        private double grade;
+
+        public Course(string courseName, string instructorName, double grade)
+        {
+            CourseName = courseName;
+            SetInstructorName(instructorName);
+            Grade = grade;
+        }
+
+        public string CourseName
+        {
+            get
+            {
+                return courseName;
+            }
+            set { }
+
+        }
+        public double Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    FormatException ex = new FormatException("Grade must be between 0 and 100");
+                }
+                else
+                {
+                    grade = value;
+                }
+            }
+        }
+        public void SetInstructorName(string instructorName)
+        {
+            if (string.IsNullOrWhiteSpace(instructorName))
+            {
+                throw new ArgumentException("Instructor name cannot be empty or whitespace.");
+            }
+            this.instructorName = instructorName;
+        }
+
+        private string CalculateLetterGrade()
+        {
+            if (grade >= 90)
+            {
+                return "A";
+            }
+            else if (grade >= 80)
+            {
+                return "B";
+            }
+            else if (grade >= 70)
+            {
+                return "C";
+            }
+            else if (grade >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+        public void PrintCourseInfo()
+
+        {
+            Console.WriteLine("Course Name: {0}", CourseName);
+            Console.WriteLine("Instructor Name: {0}", instructorName);
+            Console.WriteLine("Grade: {0}", Grade);
+            Console.WriteLine("Letter Grade: {0}", CalculateLetterGrade());
+        }
+
+    }
+}
---
+++ day 03/Q1/Q1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Course course1 = new Course("Math", "Mr. Smith", 85);
+                course1.PrintCourseInfo();
+                Console.ReadLine();
+        }
+    }
+}
